f36ad0fbfc34c318408291f1506462829c425d86
rishithareddy-036 <krishithareddy21@gmail.com>
2025-03-28 13:25:23 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,9 @@
+class Demo2
+{
+public static void main(String args[])
+{
+System.out.println("Rishitha Reddy");
+System.out.println("2451-2-737-036");
+System.out.println("MVSR Engineering College");
+}
+}
