e21d00df695dee429e72eff9f5ae7da216e6269f
rishithareddy-036 <krishithareddy21@gmail.com>
2025-07-11 12:32:47 +0530

    Add files via upload

---
+++ Loopdemo1.java
@@ -0,0 +1,14 @@
+class Loopdemo1
+{
+public static void main(String args[])
+{
+int sum=0,avg;
+for(int i=1;i<=10;i++)
+{
+sum=sum+i;
+}
+System.out.println("Sum:"+sum);
+avg=sum/10;
+System.out.println("Average:"+avg);
+}
+}
